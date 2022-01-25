b0b6e6a0156fc4bf40cc6ca913286da51752acb4
JiHoon <skyy630@naver.com>
2022-01-25 14:36:10 +0900

    01.25

--- Programmers/practice.js
+++ Programmers/practice.js
@@ -1,9 +1,9 @@
-function solution(n) {
-  let answer = String(n)
-    .split('')
-    .sort((a, b) => b - a)
-    .join('');
-  answer = Number(answer);
-  return answer;
+function solution(arr, divisor) {
+  var answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer.length == 0 ? [-1] : answer.sort((a, b) => a - b);
 }
-solution(118372);
