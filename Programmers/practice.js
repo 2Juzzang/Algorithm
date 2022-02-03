b1dffc2a70a7b080c62d0cd27cab396615be2475
JiHoon <skyy630@naver.com>
2022-02-03 17:35:16 +0900

    02.03

--- Programmers/practice.js
+++ Programmers/practice.js
@@ -1,14 +1,1 @@
-function solution(lottos, win_nums) {
-  let win = 0;
-  let zero = 0;
-  for (let i = 0; i < lottos.length; i++) {
-    if (win_nums.includes(lottos[i])) {
-      win++;
-    }
-    if (lottos[i] === 0) {
-      zero++;
-    }
-  }
-  const rank = [6, 6, 5, 4, 3, 2, 1];
-  return [rank[win + zero], rank[win]];
-}
+console.log(1 * false);
