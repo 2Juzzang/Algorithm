90017c66637dae343f224aa5de30cf562c58706d
JiHoon <skyy630@naver.com>
2022-02-04 19:18:04 +0900

    02.04

--- Programmers/practice.js
+++ Programmers/practice.js
@@ -1,1 +1,8 @@
-console.log(1 * false);
+function solution(participant, completion) {
+  for (let i = 0; i < completion.length; i++) {
+    let idx = participant.indexOf(completion[i]);
+    participant.splice(idx, 1);
+  }
+
+  return participant[0];
+}
